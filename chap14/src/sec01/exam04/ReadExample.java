e9a51206efac0b753147821f49c3f332ef46fa82
maeng0830 <kmk930830@gmail.com>
2022-01-25 22:00:38 +0900

    20220125 commit

---
+++ chap14/src/sec01/exam04/ReadExample.java
@@ -0,0 +1,20 @@
+package sec01.exam04;
+
+import java.io.*;
+
+public class ReadExample {
+
+	public static void main(String[] args) throws IOException {
+		InputStream is = new FileInputStream("C:/Temp/test1.db");
+		while(true) {
+			int data = is.read();
+			if(data == -1) break;
+			System.out.println(data);
+		}
+		
+		is.close();
+		
+
+	}
+
+}
